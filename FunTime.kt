aa3e168c5b58d5cd41f292cf966bf4f823036145
_kingFisher <55960250+fcat97@users.noreply.github.com>
2022-05-24 01:08:11 +0600

    Update FunTime.kt

--- FunTime.kt
+++ FunTime.kt
@@ -2,17 +2,17 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun main() {
-    println(make pretty (2 days earlier from today))
+    println(make pretty (2 days less from today))
 }
 
 val now: Date get() = Date()
 val today: Date get() = now
-val yesterday: Date get() = one day earlier from today
+val yesterday: Date get() = one day less from today
 val tomorrow: Date get() = one day more from today
 
 sealed class Modifier
 object more: Modifier()
-object earlier: Modifier()
+object less: Modifier()
 
 sealed class Command
 object make: Command()
@@ -25,25 +25,25 @@ object one
 infix fun one.day(x: Modifier): Long {
     return when (x) {
         is more -> 86400_000L
-        is earlier -> -1 * 86400_000L
+        is less -> -1 * 86400_000L
     }
 }
 infix fun one.hour(x: Modifier): Long {
     return when (x) {
         is more -> 3600_000L
-        is earlier -> -1 * 3600_000L
+        is less -> -1 * 3600_000L
     }
 }
 infix fun one.minute(x: Modifier): Long {
     return when (x) {
         is more -> 60_000L
-        is earlier -> -1 * 60_000L
+        is less -> -1 * 60_000L
     }
 }
 infix fun one.second(x: Modifier): Long {
     return when (x) {
         is more -> 1_000L
-        is earlier -> -1 * 1_000L
+        is less -> -1 * 1_000L
     }
 }
 
@@ -54,25 +54,24 @@ infix fun Long.from(a: Date): Date {
 infix fun Int.days(x: Modifier): Long {
     return when (x) {
         is more -> this * 86400_000L
-        is earlier -> -1 * this * 86400_000L
+        is less -> -1 * this * 86400_000L
     }
 }
-
 infix fun Int.hours(x: Modifier): Long {
     return when (x) {
         is more -> this * 3600_000L
-        is earlier -> -1 * this * 3600_000L
+        is less -> -1 * this * 3600_000L
     }
 }
 infix fun Int.minutes(x: Modifier): Long {
     return when (x) {
         is more -> this * 60_000L
-        is earlier -> -1 * this * 60_000L
+        is less -> -1 * this * 60_000L
     }
 }
 infix fun Int.seconds(x: Modifier): Long {
     return when (x) {
         is more -> this * 1_000L
-        is earlier -> -1 * this * 1_000L
+        is less -> -1 * this * 1_000L
     }
 }
